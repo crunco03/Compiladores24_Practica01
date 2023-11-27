ffbf7a83d08596771f70710636b1295c95cd462e
AlejandroRZ <alejandro_riveraz@ciencias.unam.mx>
2023-11-27 16:17:13 -0600

    Verificación modificada

--- P6/P6.hs
+++ P6/P6.hs
@@ -16,16 +16,16 @@ data Type = Num | Bool | Void deriving Show
 
 
 typeChecker :: ASA -> ASA
-typeChecker asa = if isValidType (typeCheckerAux asa)
+typeChecker asa = if isVoid (typeCheckerAux asa)
                   then asa
                   else error "El tipado del programa no es consistente o el programa no es admisible"
+
 
 typeCheckerAux :: ASA -> Type
 typeCheckerAux (Loc _) = Num
 typeCheckerAux (Number _) = Num
 typeCheckerAux (Boolean _) = Bool
 typeCheckerAux Skip = Void
-
 typeCheckerAux (Sum x y) = checkBinaryTypes x y Num
 typeCheckerAux (And p q) = checkBinaryTypes p q Bool
 typeCheckerAux (Not p) = checkUnaryType p Bool
@@ -35,17 +35,20 @@ typeCheckerAux (IfThenElse p q r) = checkIfThenElseTypes p q r
 typeCheckerAux (Seq p q) = checkSeqTypes p q
 typeCheckerAux (WhileDo p q) = checkWhileTypes p q
 
+
 checkBinaryTypes :: ASA -> ASA -> Type -> Type
 checkBinaryTypes x y expectedType
   | not (compareTypes (typeCheckerAux x) expectedType) = error $ "El tipo de " ++ show x ++ " no es el esperado"
   | not (compareTypes (typeCheckerAux y) expectedType) = error $ "El tipo de " ++ show y ++ " no es el esperado"
   | otherwise = expectedType
 
+
 checkUnaryType :: ASA -> Type -> Type
 checkUnaryType x expectedType =
     if not (compareTypes (typeCheckerAux x) expectedType)
     then error $ "El tipo de " ++ show x ++ " no es el esperado"
     else expectedType
+
 
 checkIfThenElseTypes :: ASA -> ASA -> ASA -> Type
 checkIfThenElseTypes p q r
@@ -55,9 +58,10 @@ checkIfThenElseTypes p q r
 
 checkSeqTypes :: ASA -> ASA -> Type
 checkSeqTypes p q =
-    if not (isVoid (typeCheckerAux p)) || not (isVoid (typeCheckerAux q))
-    then error $ "El tipo de los argumentos " ++ show p ++ " y " ++ show q ++ " no son los esperados"
-    else Void
+  if not (isVoid (typeCheckerAux p)) || not (isVoid (typeCheckerAux q))
+  then error $ "El tipo de los argumentos " ++ show p ++ " y " ++ show q ++ " no son los esperados"
+  else Void
+
 
 checkWhileTypes :: ASA -> ASA -> Type
 checkWhileTypes p q
@@ -65,9 +69,6 @@ checkWhileTypes p q
   | not (isVoid (typeCheckerAux q)) = error $ "El tipo de " ++ show q ++ " no es el esperado"
   | otherwise = Void
 
-isVoid :: Type -> Bool
-isVoid Void = True
-isVoid _ = False
 
 compareTypes :: Type -> Type -> Bool
 compareTypes Num Num = True
@@ -75,7 +76,6 @@ compareTypes Bool Bool = True
 compareTypes Void Void = True
 compareTypes _ _ = False
 
-isValidType :: Type -> Bool
-isValidType Num = True
-isValidType Bool = True
-isValidType Void = True
+isVoid :: Type -> Bool
+isVoid Void = True
+isVoid _ = False
